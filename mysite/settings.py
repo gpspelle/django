420ae1de7a3205fd81f92dfc8436bab35fffc699
gpspelle <gpsunicamp016@gmail.com>
2020-03-20 18:29:30 +0100

    change in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,7 +77,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('salty-brook-17226.herokuapp.com', 6379)],
         },
     },
 }
