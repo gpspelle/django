18c7889b18fb1493dc223618103e5f7447ad569b
gpspelle <gpsunicamp016@gmail.com>
2020-03-21 23:50:21 +0100

    refact

---
+++ chat/static/chat/chat.js
@@ -0,0 +1,106 @@
+var roomName = "{{ room_name|escapejs }}";
+
+	
+// When we're using HTTPS, use WSS too.
+var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+var chatSocket = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
+
+//var wss_protocol = (window.location.protocol == 'https:') ? 'wss://': 'ws://';
+//var chatSocket = new WebSocket(
+//wss_protocol + window.location.host + '/ws/chat/' + roomName + '/'
+//);
+
+chatSocket.onmessage = function(e) {
+var data = JSON.parse(e.data);
+var message = data['message'];
+document.querySelector('#chat-log').value += ('Message received: ');
+document.querySelector('#chat-log').value += (message + '\n');
+
+};
+
+chatSocket.onclose = function(e) {
+console.error('Chat socket closed unexpectedly');
+};
+
+document.querySelector('#chat-message-input').focus();
+document.querySelector('#chat-message-input').onkeyup = function(e) {
+if (e.keyCode === 13) {  // enter, return
+document.querySelector('#chat-message-submit').click();
+}
+};
+
+document.querySelector('#chat-message-submit').onclick = function(e) {
+var messageInputDom = document.querySelector('#chat-message-input');
+var message = messageInputDom.value;
+document.querySelector('#chat-log').value += ('Message sent: ');
+document.querySelector('#chat-log').value += (message + '\n');
+chatSocket.send(JSON.stringify({
+'message': message, 'recipient': 'chat_ble'
+}));
+
+messageInputDom.value = '';
+};
+
+function sendMessage(message) {
+
+document.querySelector('#chat-log').value += ('Message sent: ');
+document.querySelector('#chat-log').value += (message + '\n');
+
+chatSocket.send(JSON.stringify({
+'message': message, 'recipient': 'chat_ble'
+}));
+};
+
+</script>
+var roomName = "{{ room_name|escapejs }}";
+
+	
+// When we're using HTTPS, use WSS too.
+var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+var chatSocket = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
+
+//var wss_protocol = (window.location.protocol == 'https:') ? 'wss://': 'ws://';
+//var chatSocket = new WebSocket(
+//wss_protocol + window.location.host + '/ws/chat/' + roomName + '/'
+//);
+
+chatSocket.onmessage = function(e) {
+	var data = JSON.parse(e.data);
+	var message = data['message'];
+	document.querySelector('#chat-log').value += ('Message received: ');
+	document.querySelector('#chat-log').value += (message + '\n');
+
+};
+
+chatSocket.onclose = function(e) {
+	console.error('Chat socket closed unexpectedly');
+};
+
+document.querySelector('#chat-message-input').focus();
+document.querySelector('#chat-message-input').onkeyup = function(e) {
+	if (e.keyCode === 13) {  // enter, return
+	document.querySelector('#chat-message-submit').click();
+	}
+};
+
+document.querySelector('#chat-message-submit').onclick = function(e) {
+	var messageInputDom = document.querySelector('#chat-message-input');
+	var message = messageInputDom.value;
+	document.querySelector('#chat-log').value += ('Message sent: ');
+	document.querySelector('#chat-log').value += (message + '\n');
+	chatSocket.send(JSON.stringify({
+		'message': message, 'recipient': 'chat_ble'
+	}));
+
+	messageInputDom.value = '';
+};
+
+function sendMessage(message) {
+
+	document.querySelector('#chat-log').value += ('Message sent: ');
+	document.querySelector('#chat-log').value += (message + '\n');
+
+	chatSocket.send(JSON.stringify({
+		'message': message, 'recipient': 'chat_ble'
+	}));
+};
