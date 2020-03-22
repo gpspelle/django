37c337d39cf2f08fe712e624b1453e6eaa37fec7
gpspelle <gpsunicamp016@gmail.com>
2020-03-22 16:55:10 +0100

    a

--- chat/routing.py
+++ chat/routing.py
@@ -4,7 +4,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
 ]
 
 #websocket_urlpatterns = [
